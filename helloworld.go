335ae2927d605777175467b69b29577dc5f92102
Thomas Shafer <tshafer@google.com>
2019-06-06 22:14:46 -0700

    new change

--- helloworld.go
+++ helloworld.go
@@ -13,7 +13,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	if target == "" {
 		target = "World"
 	}
-	fmt.Fprintf(w, "Hello from thomas %s!\n", target)
+	fmt.Fprintf(w, "New Change for demo from thomas %s!\n", target)
 }
 
 func main() {
